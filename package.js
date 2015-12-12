7d62b40aa502bb97f76592b71a16945e223e94e3
DFischer <me@danielfischer.com>
2015-12-11 20:07:00 -0800

    update package.js

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'tuul:haikunator',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Generate heroku-like names',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:tuulbox/haikunator-meteor.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
